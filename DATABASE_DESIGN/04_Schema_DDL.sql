6220747831a05b7db22ffa82f0f7d4dd79eaab1f
Pallabi5200 <pallabi5200@gmail.com>
2026-07-20 11:44:28 +0530

    Added DDL for Core Pillar: Clients, Projects and Work Orders

---
+++ DATABASE_DESIGN/04_Schema_DDL.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Clients (
+    client_id VARCHAR(50) PRIMARY KEY,
+    client_name VARCHAR(255) NOT NULL,
+    billing_address TEXT NOT NULL,
+    gstin CHAR(15) NOT NULL,
+    pan_number CHAR(10) NOT NULL
+);
+
+CREATE TABLE Projects (
+    project_id VARCHAR(50) PRIMARY KEY NOT NULL,
+    client_id VARCHAR(50) NOT NULL,
+    project_name VARCHAR(255) NOT NULL,
+    site_location VARCHAR(255) NOT NULL,
+    state VARCHAR(100) NOT NULL,
+    structure_type VARCHAR(100) NOT NULL,
+    start_date DATE NOT NULL,
+    delivery_deadline DATE NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES Clients(client_id)
+);
+
+
+CREATE TABLE Work_Orders(
+    work_order_id VARCHAR(50) PRIMARY KEY NOT NULL,
+    work_order_number VARCHAR(100) NOT NULL,
+    project_id VARCHAR(50) NOT NULL,
+    issue_date DATE NOT NULL,
+    total_contract_value DECIMAL(15,2) NOT NULL,
+    payment_terms_description TEXT NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES Projects(project_id) 
+);
